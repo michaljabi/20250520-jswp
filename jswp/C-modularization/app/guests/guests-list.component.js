feee99056a8b268acdbd2cb50a65de653c9b36a0
Jabi <michal.jabi@gmail.com>
2025-05-22 10:58:25 +0200

    day2( task 3): renderIf - make it work as dom-helper.

--- jswp/C-modularization/app/guests/guests-list.component.js
+++ jswp/C-modularization/app/guests/guests-list.component.js
@@ -1,4 +1,5 @@
 import { div, h2, ul, input, button } from "../../common/dom-elements.js"
+import { renderIf } from "../../common/dom-helpers.js"
 import { GuestsListItem } from "./guests-list-item.component.js"
 
 export function GuestsList({ title = 'Lista gości', guests = [] }) {
@@ -8,7 +9,7 @@ export function GuestsList({ title = 'Lista gości', guests = [] }) {
         ul(guests.map(name => GuestsListItem({ name })), 'list-group'),
         div([
             input('form-control mx-2', { placeholder: 'wpisz imię...' }),
-            button('1', 'btn btn-primary'),
+            renderIf(() => true, button('1', 'btn btn-primary')),
         ], 'p-2 d-flex')
     ])
 }
