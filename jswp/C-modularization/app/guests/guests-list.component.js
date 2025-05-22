67cfa77774222dbcd9938ee968def1cf65801779
Jabi <michal.jabi@gmail.com>
2025-05-22 11:37:44 +0200

    task (part D): state management with callbacks initial

--- jswp/C-modularization/app/guests/guests-list.component.js
+++ jswp/C-modularization/app/guests/guests-list.component.js
@@ -2,12 +2,25 @@ import { div, h2, ul, input, button } from "../../common/dom-elements.js"
 import { renderIf } from "../../common/dom-helpers.js"
 import { IncrementButton } from "../../shared/increment-button.component.js"
 import { GuestsListItem } from "./guests-list-item.component.js"
+import { guestList } from "./guests.model.js"
 
-export function GuestsList({ title = 'Lista gości', guests = [] }) {
+export function GuestsList({ title = 'Lista gości' }) {
+
+    const guests = guestList;
+    const listRef = ul(guests.map(name => GuestsListItem({ name })), 'list-group');
+
+    // setTimeout(() => {
+    //     guests.push('test');
+    //     listRef.innerHTML = '';
+    //     listRef.append(...guests.map(name => GuestsListItem({ name })));
+    // }, 3000)
+
+    // inputRef.value
+    // btn
 
     return div([
         h2(title),
-        ul(guests.map(name => GuestsListItem({ name })), 'list-group'),
+        listRef,
         div([
             input('form-control mx-2', { placeholder: 'wpisz imię...' }),
             renderIf(() => true, IncrementButton({ value: 10, classes: 'btn btn-primary' })),
--- jswp/C-modularization/app/guests/guests.model.js
+++ jswp/C-modularization/app/guests/guests.model.js
@@ -1,1 +1,15 @@
 export const guestList = ['Marysia', 'Jacek', 'Agata', 'Zbyszek'];
+
+// 1. callback
+
+// 2. Promise (async/await)
+// 3. Streams / Observables
+
+export function onAddGuestListener(eventHandler) {
+
+}
+
+
+export function addGuest(name) {
+
+}
