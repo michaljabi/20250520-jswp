132cbf8474199bb0e57f39b5d41f7231f3b7a9f5
Jabi <michal.jabi@gmail.com>
2025-05-23 09:46:47 +0200

    explain (?.): Optional chaining operator

--- jswp/F-unny-dev-and-prod-problems/party-guests/src/main.ts
+++ jswp/F-unny-dev-and-prod-problems/party-guests/src/main.ts
@@ -1,4 +1,4 @@
 import "bootstrap/dist/css/bootstrap.css";
 import { GuestsPage } from "./app/guests/guests.page";
 
-document.querySelector<HTMLDivElement>("#app")!.append(GuestsPage());
+document.querySelector<HTMLDivElement>("#app")?.append(GuestsPage());
