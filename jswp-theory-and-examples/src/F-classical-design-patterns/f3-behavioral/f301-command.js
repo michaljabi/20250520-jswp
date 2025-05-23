aed9417cf999e371d3935af0145aba1f39ed21dd
Jabi <michal.jabi@gmail.com>
2025-05-23 14:24:23 +0200

    explain(f301): Command

--- jswp-theory-and-examples/src/F-classical-design-patterns/f3-behavioral/f301-command.js
+++ jswp-theory-and-examples/src/F-classical-design-patterns/f3-behavioral/f301-command.js
@@ -16,17 +16,17 @@ const calculator = {
 	add(num1 = 0, num2 = 0) {
 		return num1 + num2;
 	},
-	subtract(num1= 0, num2 = 0) {
+	subtract(num1 = 0, num2 = 0) {
 		return num1 - num2;
 	},
 	multiply(num1 = 0, num2 = 1) {
 		return num1 * num2;
 	},
 	divide(num1 = 0, num2 = 1) {
-		return num1/num2;
+		return num1 / num2;
 	},
-	calc(command)  {
-		if(command.type !== 'calc') {
+	calc(command) {
+		if (command.type !== 'calc') {
 			return this[command.type](command.num1, command.num2);
 		}
 		return 0;
@@ -34,13 +34,30 @@ const calculator = {
 };
 
 class Command {
-	constructor (type, num1, num2) {
+	constructor(type, num1, num2) {
 		this.type = type;
 		this.num1 = num1;
 		this.num2 = num2;
 	}
 }
 
-console.log(calculator.calc(new Command('divide', 10, 2)))
+console.log(calculator.calc(new Command('divide', 100, 2)))
 console.log(calculator.calc(new Command('subtract', 10, 2)))
 
+console.log(calculator.calc(new Command('subtract', calculator.calc(new Command('divide', 100, 2)), 2)))
+
+const SERIALIZED = [
+	{ type: 'divide', num1: 100, num2: 2 },
+	{ type: 'subtract', num2: 2 },
+]
+
+let result = 0;
+for (const action of SERIALIZED) {
+	if (action.num1) {
+		result = calculator.calc(action)
+	} else {
+		result = calculator.calc({ ...action, num1: result })
+	}
+}
+
+console.log(result);
